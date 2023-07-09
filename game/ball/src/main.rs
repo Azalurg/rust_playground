2f39a996e6c41d3b80b2ddd0cd4870d8beae0cde
Azalurg <91213389+Azalurg@users.noreply.github.com>
2023-07-09 18:48:29 +0200

    Player movement

--- game/ball/src/main.rs
+++ game/ball/src/main.rs
@@ -1,16 +1,25 @@
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 
+pub const PLAYER_SIZE: f32 = 64.0;
+pub const PLAYER_SPEED: f32 = 500.0;
+pub const NUMBER_OF_ENEMIES: usize = 4;
+
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
     .add_startup_system(spawn_player)
     .add_startup_system(spawn_camera)
+    .add_system(player_movement)
+    .add_system(confine_player_movement)
     .run();
 }
 
 #[derive(Component)]
 pub struct Player {}
+
+#[derive(Component)]
+pub struct Enemy {}
 
 pub fn spawn_player(
     mut commands: Commands,
@@ -38,4 +47,69 @@ pub fn spawn_camera(
         transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
         ..default()
     });
+}
+
+pub fn spawn_enemies(){
+    
+}
+
+
+pub fn player_movement(    
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+){
+    if let Ok(mut transform) = player_query.get_single_mut(){
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::W){
+            direction += Vec3::new(0.0, 1.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::A){
+            direction += Vec3::new(-1.0, 0.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::S){
+            direction += Vec3::new(0.0, -1.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::D){
+            direction += Vec3::new(1.0, 0.0, 0.0);
+        }
+
+        if direction.length() > 0.0{
+            direction = direction.normalize();
+        }
+
+        transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    }
+}
+
+pub fn confine_player_movement(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>
+){
+    if let Ok(mut player_transform) = player_query.get_single_mut(){
+        let window = window_query.get_single().unwrap();
+
+        let half_player_size = PLAYER_SIZE / 3.0;
+        let x_min = 0.0 + half_player_size;
+        let x_max = window.width() - half_player_size;
+        let y_min = 0.0 + half_player_size;
+        let y_max = window.height() - half_player_size;
+
+        let mut translation = player_transform.translation;
+
+        if translation.x < x_min{
+            translation.x = x_min
+        } else if translation.x > x_max {
+            translation.x = x_max
+        }
+
+        if translation.y < y_min{
+            translation.y = y_min
+        } else if translation.y > y_max {
+            translation.y = y_max
+        }
+
+        player_transform.translation = translation;
+    }
 }
