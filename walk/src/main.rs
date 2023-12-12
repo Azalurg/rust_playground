582e096a209e57a70a168dd08e68b7e75dce1840
Azalurg <91213389+Azalurg@users.noreply.github.com>
2023-12-12 18:08:12 +0100

    count all extentions

--- walk/src/main.rs
+++ walk/src/main.rs
@@ -1,7 +1,7 @@
 // use walkdir::WalkDir;
 // use id3::{Tag, TagLike};
 
-// const PATH: &str = "/mnt/samba/Books";
+// const PATH: &str = "...";
 
 // fn is_hidden(entry: &walkdir::DirEntry) -> bool {
 //     entry
@@ -32,35 +32,59 @@
 //     }
 // }
 
-use std::fs::File;
-use std::io::Write;
+// use std::fs::File;
+// use std::io::Write;
 
-use rust_embed::RustEmbed;
-use tempfile::TempDir;
+// use rust_embed::RustEmbed;
+// use tempfile::TempDir;
+// use walkdir::WalkDir;
+
+// #[derive(RustEmbed)]
+// #[folder = "data/"]
+// struct Asset;
+
+// fn main(){
+
+//     let tem_dir = TempDir::new().unwrap();
+//     let templates_path = tem_dir.path();
+
+//     for file in Asset::iter() {
+//         let mut temp_file = File::create(templates_path.join(file.as_ref())).unwrap();
+
+//         let file_str = std::str::from_utf8(Asset::get(file.as_ref()).unwrap().data.as_ref()).unwrap().to_string();
+//         writeln!(temp_file, "{}", file_str);
+
+//         print!("{file_str}")
+//     }
+
+//     let walker = WalkDir::new(templates_path).into_iter();
+
+//     for entery in walker{
+//                 println!("{}", entery.unwrap().path().display());
+//             }
+
+// }
+
+use std::collections::HashMap;
+
 use walkdir::WalkDir;
+fn main() {
+    let path = "...";
+    let mut dict = HashMap::new();
 
-#[derive(RustEmbed)]
-#[folder = "data/"]
-struct Asset;
-
-fn main(){
-
-    let tem_dir = TempDir::new().unwrap();
-    let templates_path = tem_dir.path();
-
-    for file in Asset::iter() {
-        let mut temp_file = File::create(templates_path.join(file.as_ref())).unwrap();
-        
-        let file_str = std::str::from_utf8(Asset::get(file.as_ref()).unwrap().data.as_ref()).unwrap().to_string();
-        writeln!(temp_file, "{}", file_str);
-        
-        print!("{file_str}")
+    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+        if entry.file_type().is_file() {
+            match entry.path().extension() {
+                Some(ext) => {
+                    let count = dict.entry(ext.to_str().unwrap().to_string()).or_insert(0);
+                    *count += 1;
+                }
+                None => (),
+            }
+        }
     }
 
-    let walker = WalkDir::new(templates_path).into_iter();
-
-    for entery in walker{
-                println!("{}", entery.unwrap().path().display());
-            }
-
+    for (key, value) in dict.iter() {
+        println!("{}: {}", key, value);
+    }
 }
